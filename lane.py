f01840ba9d902e9037801b111fe88db6c5828a1e
matt <mwb7c@virginia.edu>
2010-09-09 13:04:11 -0400

    Pre major overhaul of look ahead, etc in prep for adding chaning lane support

--- lane.py
+++ lane.py
@@ -9,31 +9,32 @@ class Lane:
 	# Lane_becomes is the lane that self turn into.
 	# It equals 0 if the lane ends.
 	# It equals None if the lane dissapears into the ether.
-	def __init__(self, length, lane_becomes=None, rlane=None, llane=None):
+	def __init__(self, length, nlane=None, slane=None, elane=None, wlane=None):
 		self.length = length
-		self._car_queue = deque([])
-		self._lane_becomes = lane_becomes
-		self.rlane = rlane
-		self.llane = llane
+		self.car_queue = deque([])
+		self.nlane = nlane
+		self.slane = slane
+		self.elane = elane
+		self.wlane = wlane
 
-	def change_lane_becomes(new_lane_becomes):
-		"""Chanes the self._lane_becomes value.
-		Should only be used when reading init file"""
-		self._lane_becomes = new_lane_becomes
-	
 	def add_car(self, car):
-		self._car_queue.appendleft(car)
+		if car.offset >= self.length:
+			if self.nlane != None:
+				car.offset = car.offset - self.length
+				self.nlane.add_car(car)
+		else:
+			self.car_queue.appendleft(car)
 
 	def get_info_ahead(self, is_cur_lane=False):
-		if (len(self._car_queue) != 0) and (not is_cur_lane):
-			lead = self._car_queue[0]
+		if (len(self.car_queue) != 0) and (not is_cur_lane):
+			lead = self.car_queue[0]
 			return (lead.offset, lead.vel, lead.length)
-		elif self._lane_becomes == None:
+		elif self.nlane == None:
 			return (None, 0, 0)
-		elif self._lane_becomes == 0:
+		elif self.nlane == 0:
 			return (self.length, 0, 0)
 		else:
-			info = self._lane_becomes.get_info_ahead()
+			info = self.nlane.get_info_ahead()
 			if info[0] == None:
 				return (None, info[1], info[2])
 			else:
@@ -41,32 +42,33 @@ class Lane:
 
 	# Updates the lane.
 	def update_lane(self):
-		num_cars = len(self._car_queue)
+		num_cars = len(self.car_queue)
 		if num_cars == 0:
 			return
 		for index in range(num_cars - 1):
-			follow = self._car_queue[index]
-			lead = self._car_queue[index + 1]
+			follow = self.car_queue[index]
+			lead = self.car_queue[index + 1]
 			follow.update_car(lead.offset, lead.vel, lead.length)
 		lead_info = self.get_info_ahead(True)
-		self._car_queue[-1].update_car(*lead_info)
+		self.car_queue[-1].update_car(*lead_info)
 
 	def check_offsets(self):
 		count = 0
-		for car in reversed(self._car_queue):
-			if car.offset > self.length:
+		for car in reversed(self.car_queue):
+			if car.offset >= self.length:
 				count = count + 1
 			else:
 				break
 		for foo in range(count):
-			car = self._car_queue.pop()
-			if self._lane_becomes != None:
+			car = self.car_queue.pop()
+			if self.nlane != None:
 				car.offset = car.offset - self.length
-				self._lane_becomes.add_car(car)
+				self.nlane.add_car(car)
 
 	def print_lane(self):
 		"""Print out all of the car's id, offset, and vel"""
-		for car in reversed(self._car_queue):
+		print "        --LANE--"
+		for car in reversed(self.car_queue):
 			print "\tCar", car.id
 			print "Pos", car.offset, "m"
 			print "Speed", car.vel, "m/s"
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import car
 import lane
 
 lane0 = lane.Lane(1000, 0)
-lane1 = lane.Lane(1600, lane0)
+lane1 = lane.Lane(700, lane0)
 
 lane1.add_car(car.Car(0, 2, 0, 20, 6.5, 2/3.0, -3, -3.5))
-for step in range(1,300):
-	if lane1._car_queue[0].offset > 6.5:
+for step in range(1,100):
+	if lane1.car_queue[0].offset > 6.5:
 		lane1.add_car(car.Car(step, 2, 0, 29, 6.5, 2/3.0, -3, -3.5))
 	print "*****STEP", step, "*****"
 	lane0.print_lane()
@@ -19,4 +19,5 @@ for step in range(1,300):
 	lane1.update_lane()
 	lane0.update_lane()
 
+	lane1.check_offsets()
 	lane0.check_offsets()
