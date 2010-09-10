c43f7d3f43b52bbb65a427daaa475351576d608d
matt <mwb7c@virginia.edu>
2010-09-09 22:51:43 -0400

    Got partway through lane change coding and relized I want to code lane change entirely different. Instead of suprising the back veh in the target lane, all of my vehs are gonna put on turn signals one prt before making the manuever.

--- car.py
+++ car.py
@@ -2,6 +2,8 @@
 # Mar 25, 2010
 # car.py
 
+import copy
+
 class Car:
 	def __init__(self, id, max_accel, vel, desired_vel, length, prt,
 					max_brake, obs_brake):
@@ -15,21 +17,39 @@ class Car:
 		self.obs_brake = obs_brake
 		self.offset = 0
 	
-	def update_car(self, ninfo, einfo, winfo):
-		vel = self.step_vel(*ninfo)
-		if einfo is not None:
-			ndist, nvel, sdist, scar = einfo
-			dist = ndist + sdist + self.length
-			evel = scar.step_vel(ndist, nvel)
-		if winfo is not None:
-			ndist, nvel, sdist, scar = winfo
-			dist = ndist + sdist + self.length
-			wvel = self.step_vel(*winfo)
+	def update_car(self, n_info, e_info, w_info):
+		vel = self.step_vel(*n_info)
+		if e_info is not None:
+			n_dist, n_vel, s_dist, s_car = e_info
+			car_vel = self.step_vel(n_dist, n_vel)
+
 		if vel < 0.01:
 			vel = 0
 		self.vel = vel
 		self.offset = self.offset + self.vel * self.prt
 		assert self.vel >= 0
+
+	def test_step(self, s_car, n_dist, s_dist, car_vel):
+		if s_car is None:
+			return 0
+
+		test_car = copy.copy(s_car)
+		test_vel = test_car.step_vel(n_dist + s_dist, n_vel)
+		if test_vel < 0.01:
+			test_vel = 0
+		test_car.vel = test_vel
+		test_car.offset = test_car.offset + test_car.vel * test_car.prt
+
+		car_offset = self.offset + car_vel * self.prt
+		if test_car.offset > car_offset:
+			return None
+		test_vel = test_car.step_vel(car_offset - test_car.offset, car_vel)
+		if test_vel < 0.01:
+			test_vel = 0
+		if (test_vel - test_car.vel) < test_car.max_brake:
+			return None
+		return test_car.vel test_vel
+		test_car.offset = test_car.offset + test_car.vel * test_car.prt
 
 	def step_vel(self, lead_dist=None, lead_vel=0):
 		"""Get the new velocity depending on the lead car using the Gipps Model.
--- lane.py
+++ lane.py
@@ -10,20 +10,20 @@ class Lane:
 	# Lane_becomes is the lane that self turn into.
 	# It equals 0 if the lane ends.
 	# It equals None if the lane dissapears into the ether.
-	def __init__(self, length, nlane=None, slane=None, elane=None, wlane=None):
+	def __init__(self, length, n_lane=None, s_lane=None, e_lane=None, w_lane=None):
 		self.length = length
 		self.car_queue = deque([])
 		self.refrence_queue = deque([])
-		self.nlane = nlane
-		self.slane = slane
-		self.elane = elane
-		self.wlane = wlane
+		self.n_lane = n_lane
+		self.s_lane = s_lane
+		self.e_lane = e_lane
+		self.w_lane = w_lane
 
 	def add_car(self, car):
 		if car.offset >= self.length:
-			if self.nlane != None:
+			if self.n_lane != None:
 				car.offset = car.offset - self.length
-				self.nlane.add_car(car)
+				self.n_lane.add_car(car)
 		else:
 			self.car_queue.appendleft(car)
 
@@ -31,11 +31,11 @@ class Lane:
 		for car in self.refrence_queue:
 			if car.offset > offset:
 				return [car.offset - car.length - offset, car.vel]
-		if self.nlane is None:
+		if self.n_lane is None:
 			return [None, 0]
-		if self.nlane == 0:
+		if self.n_lane == 0:
 			return [self.length - offset, 0]
-		dist, vel = self.nlane.get_info_ahead(-1)
+		dist, vel = self.n_lane.get_info_ahead(-1)
 		if dist is not None:
 			if offset == -1:
 				offset = 0
@@ -46,9 +46,9 @@ class Lane:
 		for car in reversed(self.refrence_queue):
 			if car.offset <= offset:
 				return [offset - car.offset, car]
-		if self.nlane is None:
+		if self.n_lane is None:
 			return [None, 0]
-		dist, car = self.slane.get_info_behind(self.slane.length)
+		dist, car = self.s_lane.get_info_behind(self.s_lane.length)
 		if dist is not None:
 			dist = dist + offset
 		return [dist, car]
@@ -57,17 +57,17 @@ class Lane:
 	def update_lane(self):
 		for car in self.car_queue:
 			ninfo = self.get_info_ahead(car.offset)
-			if self.elane is None:
-				einfo = None
+			if self.e_lane is None:
+				e_info = None
 			else:
-				einfo = (self.elane.get_info_ahead(car.offset) +
-						self.elane.get_info_behind(car.offset - car.length))
-			if self.wlane is None:
-				winfo = None
+				e_info = (self.e_lane.get_info_ahead(car.offset) +
+						self.e_lane.get_info_behind(car.offset - car.length))
+			if self.w_lane is None:
+				w_info = None
 			else:
-				winfo = (self.wlane.get_info_ahead(car.offset) +
-						self.wlane.get_info_behind(car.offset - car.length))
-			car.update_car(ninfo, einfo, winfo)
+				w_info = (self.w_lane.get_info_ahead(car.offset) +
+						self.w_lane.get_info_behind(car.offset - car.length))
+			car.update_car(ninfo, e_info, w_info)
 
 	def check_offsets(self):
 		count = 0
@@ -78,9 +78,9 @@ class Lane:
 				break
 		for foo in range(count):
 			car = self.car_queue.pop()
-			if self.nlane != None:
+			if self.n_lane != None:
 				car.offset = car.offset - self.length
-				self.nlane.add_car(car)
+				self.n_lane.add_car(car)
 
 	def print_lane(self):
 		"""Print out all of the car's id, offset, and vel"""
